d6930848d9f5409ad1501cbd73c7a7fea815f94d
George Moon <george@moon.org.nz>
2017-04-30 23:05:01 +1200

    Added StudyArea slug and relevant tests

--- src/locations/models.py
+++ src/locations/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.utils.text import slugify
 
 class StudyArea(models.Model):
-    name = models.CharField(max_length=200)
-    
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=50, primary_key=True, editable=False)
+
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        """ Generate slug from name """
+        self.slug = slugify(self.name)
+        super(StudyArea, self).save(*args, **kwargs)
--- src/locations/tests.py
+++ src/locations/tests.py
@@ -1,3 +1,48 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+from .models import StudyArea
+
+class StudyAreaObjectTests(TestCase):
+    """ Tests for main functions of StudyArea objects """
+    def test_blank(self):
+        """ The model should not submit if all fields are left blank """
+        with self.assertRaises(ValidationError):
+            study_area = StudyArea()
+            study_area.full_clean()
+            study_area.save()
+
+
+    def test_unique(self):
+        """ Models should be unique (on name field) """
+        with self.assertRaises(ValidationError):
+            study_area = StudyArea(name='Christchurch')
+            study_area_duplicate = StudyArea(name='Christchurch')
+
+            study_area.full_clean()
+            study_area.save()
+
+            study_area_duplicate.full_clean()
+            study_area_duplicate.save()
+
+    def test_unique(self):
+        """ Models should be unique (on name field) """
+        with self.assertRaises(ValidationError):
+            study_area = StudyArea(name='Christchurch')
+            study_area_duplicate = StudyArea(name='Christchurch')
+
+            study_area.full_clean()
+            study_area.save()
+
+            study_area_duplicate.full_clean()
+            study_area_duplicate.save()
+
+    def test_slug_generated(self):
+        """ Models should auto-generate a slug on save """
+
+        study_area = StudyArea(name='Christchurch')
+        self.assertEqual(study_area.slug, '')
+
+        study_area.full_clean()
+        study_area.save()
+        self.assertEqual(study_area.slug, 'christchurch')
