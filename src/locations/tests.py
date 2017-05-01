e3c11d88ade9d578f990ab6eb8bc6a0a7de6955f
George Moon <george@moon.org.nz>
2017-05-01 12:35:01 +1200

    Removed duplicate test for locations

--- src/locations/tests.py
+++ src/locations/tests.py
@@ -5,25 +5,13 @@ from .models import StudyArea
 
 class StudyAreaObjectTests(TestCase):
     """ Tests for main functions of StudyArea objects """
+
     def test_blank(self):
         """ The model should not submit if all fields are left blank """
         with self.assertRaises(ValidationError):
             study_area = StudyArea()
             study_area.full_clean()
             study_area.save()
-
-
-    def test_unique(self):
-        """ Models should be unique (on name field) """
-        with self.assertRaises(ValidationError):
-            study_area = StudyArea(name='Christchurch')
-            study_area_duplicate = StudyArea(name='Christchurch')
-
-            study_area.full_clean()
-            study_area.save()
-
-            study_area_duplicate.full_clean()
-            study_area_duplicate.save()
 
     def test_unique(self):
         """ Models should be unique (on name field) """
