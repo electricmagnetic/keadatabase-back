34d74fff935a9f8fabccaff3870fda1a9ffc989c
George Moon <george@moon.org.nz>
2017-05-31 11:58:47 +1200

    Added initial bird fields

--- src/birds/models.py
+++ src/birds/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+from locations.models import StudyArea
+
+SEX_CHOICES = (
+    ('', 'Undetermined'),
+    ('F', 'Female'),
+    ('M', 'Male'),
+)
+
+LIFE_STAGE_CHOICES = (
+    ('', 'Unknown'),
+    ('A', 'Adult'),
+    ('S', 'Sub-adult'),
+    ('J', 'Juvenile'),
+    ('F', 'Fledgling'),
+)
+
+STATUS_CHOICES = (
+    ('', 'Unknown'),
+    ('+', 'Alive'),
+    ('-', 'Dead'),
+)
+
+class Bird(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, primary_key=True, editable=False)
+
+    sex = models.CharField(max_length=1, blank=True, choices=SEX_CHOICES,
+                           default='')
+    status = models.CharField(max_length=1, blank=True, choices=STATUS_CHOICES,
+                              default='')
+    birthday = models.DateField(blank=True, null=True)
+
+    study_area = models.ForeignKey(StudyArea, blank=True, null=True)
+
+    date_modified = models.DateTimeField(auto_now=True)
+    date_imported = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """ Generate slug from name """
+        self.slug = slugify(self.name)
+        super(Bird, self).save(*args, **kwargs)
--- src/birds/tests.py
+++ src/birds/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+from .models import Bird
+
+class BirdObjectTests(TestCase):
+    """ Tests for main functions of Bird objects """
+
+    def test_blank(self):
+        """ The model should not submit if all fields are left blank """
+        with self.assertRaises(ValidationError):
+            bird = Bird()
+            bird.full_clean()
+            bird.save()
+
+    def test_unique(self):
+        """ Models should be unique (on name field) """
+        with self.assertRaises(ValidationError):
+            bird = Bird(name='Helen Clark')
+            bird_duplicate = Bird(name='Helen Clark')
+
+            bird.full_clean()
+            bird.save()
+
+            bird_duplicate.full_clean()
+            bird_duplicate.save()
+
+    def test_slug_generated(self):
+        """ Models should auto-generate a slug on save """
+        bird = Bird(name='Helen Clark')
+        self.assertEqual(bird.slug, '')
+
+        bird.full_clean()
+        bird.save()
+        self.assertEqual(bird.slug, 'helen-clark')
