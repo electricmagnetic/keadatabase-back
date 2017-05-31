14a789f9afd708292c35f8584b5e1c24d6e0102c
George Moon <george@moon.org.nz>
2017-05-31 13:07:29 +1200

    Code tidy

--- src/locations/models.py
+++ src/locations/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.text import slugify
 
 class StudyArea(models.Model):
+    """ Basic location information, designed to be imported from Access """
+
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=50, primary_key=True, editable=False)
 
@@ -13,5 +15,6 @@ class StudyArea(models.Model):
 
     def save(self, *args, **kwargs):
         """ Generate slug from name """
+
         self.slug = slugify(self.name)
         super(StudyArea, self).save(*args, **kwargs)
