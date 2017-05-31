68f971252e6ce5f34c41904ea0a12e874379d5bf
George Moon <georgemoon@users.noreply.github.com>
2017-05-31 22:51:26 +1200

    Added Bird model (#14)
    * Added initial bird fields
    
    * Added age and life stage functions (with tests) #6 #12
    
    * Code tidy
    
    * Added annotations to Bird model tests

--- src/birds/models.py
+++ src/birds/models.py
@@ -1,3 +1,86 @@
-from django.db import models
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+from locations.models import StudyArea
+
+SEX_CHOICES = (
+    ('', 'Undetermined'),
+    ('F', 'Female'),
+    ('M', 'Male'),
+)
+
+LIFE_STAGE_CHOICES = (
+    ('', 'Unknown'),
+    ('A', 'Adult'),
+    ('S', 'Sub-adult'),
+    ('J', 'Juvenile'),
+    ('F', 'Fledgling'),
+)
+
+STATUS_CHOICES = (
+    ('', 'Unknown'),
+    ('+', 'Alive'),
+    ('-', 'Dead'),
+)
+
+class Bird(models.Model):
+    """ Basic bird information, designed to be imported from Access """
+
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.CharField(max_length=100, primary_key=True, editable=False)
+
+    sex = models.CharField(max_length=1, blank=True, choices=SEX_CHOICES,
+                           default='')
+    status = models.CharField(max_length=1, blank=True, choices=STATUS_CHOICES,
+                              default='')
+    birthday = models.DateField(blank=True, null=True)
+
+    study_area = models.ForeignKey(StudyArea, blank=True, null=True)
+
+    date_modified = models.DateTimeField(auto_now=True)
+    date_imported = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        """ Generate slug from name """
+
+        self.slug = slugify(self.name)
+        super(Bird, self).save(*args, **kwargs)
+
+    def get_age(self):
+        """ Calculates age based on birthday """
+
+        if self.status == '-':  # No age if bird is dead
+            return None
+        if self.birthday == None:  # No age if no birthday
+            return None
+
+        difference_in_years = relativedelta(date.today(), self.birthday).years
+        return difference_in_years
+
+    def get_life_stage(self):
+        """ Calculates life stage based on age (integers) """
+
+        if self.get_age() == None:
+            return None
+
+        age = self.get_age()
+
+        if age == 0:  # Under 12 months
+            return 'Fledgling'
+        elif age == 1:
+            return 'Juvenile'
+        elif age >= 2 and age < 4:
+            return 'Sub-Adult'
+        elif age >= 4:
+            return 'Adult'
+        else:
+            return None
--- src/birds/tests.py
+++ src/birds/tests.py
@@ -1,3 +1,86 @@
-from django.test import TestCase
+from datetime import date, timedelta
 
-# Create your tests here.
+from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+from .models import Bird
+
+class BirdObjectTests(TestCase):
+    """ Tests for main functions of Bird objects """
+
+    def test_blank(self):
+        """ The model should not submit if all fields are left blank """
+        with self.assertRaises(ValidationError):
+            bird = Bird()
+            bird.full_clean()
+            bird.save()
+
+    def test_unique(self):
+        """ Models should be unique (on name field) """
+        with self.assertRaises(ValidationError):
+            bird = Bird(name='Helen Clark')
+            bird_duplicate = Bird(name='Helen Clark')
+
+            bird.full_clean()
+            bird.save()
+
+            bird_duplicate.full_clean()
+            bird_duplicate.save()
+
+    def test_slug_generated(self):
+        """ Models should auto-generate a slug on save """
+        bird = Bird(name='Helen Clark')
+        self.assertEqual(bird.slug, '')
+
+        bird.full_clean()
+        bird.save()
+        self.assertEqual(bird.slug, 'helen-clark')
+
+class BirdFunctionTests(TestCase):
+    """ Tests for custom functions of Bird objects """
+
+    def test_get_age(self):
+        """ Function should return correct age for 600-day-old bird """
+        birthday = date.today() - timedelta(days=600)
+        bird = Bird(name='Helen Clark', birthday=birthday)
+
+        bird.full_clean()
+        bird.save()
+
+        self.assertEqual(bird.get_age(), 1)
+
+    def test_no_get_age(self):
+        """ Function should return None if no birthday """
+        bird = Bird(name='Helen Clark', birthday=None)
+
+        bird.full_clean()
+        bird.save()
+
+        self.assertEqual(bird.get_age(), None)
+
+    def test_get_life_stage(self):
+        """ Function should return different stages depending on birthday """
+        birthday = date.today() - timedelta(days=100)
+        bird_fledgling = Bird(name='Helen Clark', birthday=birthday)
+
+        bird_fledgling.full_clean()
+        bird_fledgling.save()
+
+        self.assertEqual(bird_fledgling.get_life_stage(), 'Fledgling')
+
+        birthday = date.today() - timedelta(days=600)
+        bird_juvenile = Bird(name='Keith Moon', birthday=birthday)
+
+        bird_juvenile.full_clean()
+        bird_juvenile.save()
+
+        self.assertEqual(bird_juvenile.get_life_stage(), 'Juvenile')
+
+    def test_no_get_life_stage(self):
+        """ Function should return None if no birthday """
+        bird = Bird(name='Helen Clark', birthday=None)
+
+        bird.full_clean()
+        bird.save()
+
+        self.assertEqual(bird.get_life_stage(), None)
