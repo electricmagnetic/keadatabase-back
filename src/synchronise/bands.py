b17ee97b440854cfa5615c7d4a93c219b784a07d
George Moon <george@moon.org.nz>
2017-06-16 12:48:55 +1200

    WIP: Added structure to band synchornisation code (but non-functional)

--- src/synchronise/bands.py
+++ src/synchronise/bands.py
@@ -5,25 +5,47 @@ from django.utils import timezone
 from django.core import management
 from django.utils.text import slugify
 
+from birds.models import Bird
+
 def is_valid_Band(row):
-    if row['Action'].lower() == 'deployed':
+    """ Returns True if given row appears to be a Band, False otherwise """
+
+    action = row['Action'].lower()
+    if action == 'deployed':
+        return True
+    elif action == 'transferred':
         return True
 
     return False
 
+def get_Bird(name):
+    """ Returns a Bird object if it matches the given name, False otherwise """
+    
+    # check that bird for the combo exists
+    # (verify where there is band but no bird, and bird but no band)
+
+    pass
+
 def classify_Band(row):
     """ Returns type of Band, if Band - filters noise in original dataset """
 
-    # Check whether band is either colour or letter combo
-    if row['DeviceClass'] == 'Colour combo':
-        return 'colour'
-    if row['DeviceClass'] == 'Letter combo':
-        return 'letter'
+    # determine what is noise and what is not
+    # split out area and band id
+    # categorise based on 'new' style or 'old' style
 
-    return False
+    pass
+
+def standardise_Band(row):
+    """ Takes a given row and returns a standardised object that can be created """
+
+    band = []
+    classification = classify_Band(row)
+    bird = get_Bird(name)
+
+    return band
 
 def synchronise_Band(self, transmitters_file):
-    """ Imports Band objects from data/Transmitters actions.csv """
+    """ Imports Band objects from data/Transmitter actions.csv """
 
     if hasattr(self, 'stdout'):
         self.stdout.write(self.style.MIGRATE_LABEL("Band:"))
@@ -38,12 +60,11 @@ def synchronise_Band(self, transmitters_file):
             if not is_valid_Band(row):
                 continue
 
-            print ('"%s", "%s", "%s", "%s"' % (row['Kea ID'], row['Transmitter ID'].rsplit(None, 1)[0], row['Transmitter ID'].rsplit(None, 1)[-1], row['Date']))
-            checked_count += 1
+            band = standardise_Band(row)
+            # add band as Django object
 
-            #band_type = classify_Band(row)
+            #print ('"%s", "%s", "%s", "%s"' % (row['Kea ID'], row['Transmitter ID'].rsplit(None, 1)[0], row['Transmitter ID'].rsplit(None, 1)[-1], row['Date']))
 
-            #print(band_type)
 
     if hasattr(self, 'stdout'):
         self.stdout.write("\tChecked: %d" % checked_count)
