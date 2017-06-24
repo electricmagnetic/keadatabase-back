e67329f5792677fc19bd5a01ea4caec84ddaf706
George Moon <george@moon.org.nz>
2017-06-24 15:44:46 +1200

    Updates to requirements, settings, static files and favicons in preparation for Heroku deploy

--- src/keadatabase/settings.py
+++ src/keadatabase/settings.py
@@ -133,6 +133,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 # Media files
