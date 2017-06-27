f143216967d8f72e7be53c4fb540d2c4578b8912
George Moon <george@moon.org.nz>
2017-06-27 21:54:15 +1200

    Added BandCombo standardisation code

--- src/synchronise/bands.py
+++ src/synchronise/bands.py
@@ -1,5 +1,6 @@
 import csv
 import datetime
+import re
 
 from django.utils import timezone
 from django.core import management
@@ -8,6 +9,9 @@ from django.utils.text import slugify
 from locations.models import StudyArea
 from birds.models import Bird
 from bands.models import BandCombo
+
+COLOURS = ['yellow', 'white', 'metal', 'blue', 'red', 'orange', 'silver',
+           'pink', 'black', 'green', 'grey', 'lime', 'purple',]
 
 def get_StudyArea(row):
     """ Returns a StudyArea if it matches the name obtained from the row, False otherwise """
@@ -59,9 +63,7 @@ def is_valid_BandCombo(row):
         return False
 
     # must have valid colour
-    colours = ['yellow', 'white', 'metal', 'blue', 'red', 'orange', 'silver',
-               'pink', 'black', 'green', 'grey',]
-    if not any(colour in row['Transmitter ID'].lower() for colour in colours):
+    if not any(colour in row['Transmitter ID'].lower() for colour in COLOURS):
         return False
 
     # must have a date
@@ -84,30 +86,83 @@ def is_valid_BandCombo(row):
 
     # TODO: (verify where there is band but no bird, and bird but no band)
 
-def standardise_BandCombo(row):
-    """ Takes a given row and returns a standardised object """
+def standardise_BandCombo(row, bird, study_area):
+    """ Takes a given row, bird and study_area and returns a standardised object """
 
-    band_combo = []
-    classification = classify_BandCombo(row)
-    bird = get_Bird(name)
+    standardised_bc = {
+        'bird': bird,
+        'study_area': study_area,
+        'date_deployed': datetime.datetime.strptime(row['Date'], "%Y-%m-%d %H:%M:%S"),
+    }
 
-    # determine what is noise and what is not
-    # split out area and band id
-    # categorise based on 'new' style or 'old' style
-    # write tests
-    # special characters (e.g. divides)
-    # decommissioned bands
-    # deploy – check date deployed, use most recent one
+    raw_bc_str = row['Transmitter ID']
 
-    # if not get_StudyArea(one_word_location) and \
-    #    not get_StudyArea(two_word_location) and \
-    #    not one_word_location == 'Decommissioned' and \
-    #    not one_word_location == 'duplicate' and \
-    #    not one_word_location == '2' and \
-    #    not one_word_location == '3':
-    #     # TODO: check the special cases
+    # [common] remove identified StudyArea from raw string
+    raw_bc_str = raw_bc_str.replace(str(study_area), "")
 
-    return band_combo
+    # [common] determine style (new/old) - looks for 'on' as a standalone word
+    if re.search(r'\bon\b', raw_bc_str.lower()) is not None:
+        standardised_bc['style'] = 'new'
+    else:
+        standardised_bc['style'] = 'old'
+
+    # [common] identify colours
+    standardised_bc['colours'] = []
+    for colour in COLOURS:
+        if colour in raw_bc_str.lower():
+            standardised_bc['colours'].append(colour)
+
+    # [common] remove extraneous words (case insensitive)
+    old_extraneous_words = ['decommissioned', '2', '3',]
+    new_extraneous_words = ['big', 'duplicate', '(upsidedown m)', 'm -']
+
+    if standardised_bc['style'] == 'new':
+        for word in new_extraneous_words:
+            re_word = re.compile(re.escape(word), re.IGNORECASE)
+            raw_bc_str = re_word.sub('', raw_bc_str)
+    else:
+        for word in old_extraneous_words:
+            re_word = re.compile(re.escape(word), re.IGNORECASE)
+            raw_bc_str = re_word.sub('', raw_bc_str)
+
+    # [common] ensure '/' symbols are surrounded by whitespace
+    raw_bc_str = raw_bc_str.replace('/', ' / ')
+
+    # [common] ensure whitespace is standardised to single spaces
+    raw_bc_str = " ".join(raw_bc_str.split())
+
+    # [new] identify symbols - put all data in array, filter out other stuff
+    standardised_bc['symbols'] = []
+
+    if standardised_bc['style'] == 'new':
+        raw_symbols = raw_bc_str.split()
+
+        for raw_symbol in list(raw_symbols):
+            # list(...) used so as to not delete items from currently iterating list
+            # Remove any colours, 'on' and '/'
+            if raw_symbol.lower() in COLOURS or \
+               raw_symbol.lower() == 'on' or \
+               raw_symbol.lower() == '/':
+                raw_symbols.remove(raw_symbol)
+
+        # remove duplicates (e.g ['A', 'A'])
+        standardised_bc['symbols'] = list(set(raw_symbols))
+
+    # [common] string now suitably processed
+    standardised_bc['name'] = raw_bc_str
+
+    # [common] offer alternative to special characters
+    standardised_bc['special'] = ''
+    if '◊' in standardised_bc['name']:
+        standardised_bc['special'] = 'diamond'
+    elif 'Σ' in standardised_bc['name']:
+        standardised_bc['special'] = 'sigma'
+    elif '÷' in standardised_bc['name']:
+        standardised_bc['special'] = 'divides'
+    elif 'θ' in standardised_bc['name']:
+        standardised_bc['special'] = 'theta'
+
+    return standardised_bc
 
 def synchronise_BandCombo(self, transmitters_file):
     """ Imports Band objects from data/Transmitter actions.csv """
@@ -125,19 +180,12 @@ def synchronise_BandCombo(self, transmitters_file):
             if not is_valid_BandCombo(row):
                 continue
 
-            #band_combo = standardise_BandCombo(row)
-
             # Get associated objects
             bird = get_Bird(row)
             study_area = get_StudyArea(row)
 
-            # Map fields
-            band_combo_map = {
-                'bird': bird,
-                'name': row['Transmitter ID'],
-                'study_area': study_area,
-                'date_deployed': datetime.datetime.strptime(row['Date'], "%Y-%m-%d %H:%M:%S")
-            }
+            # Get standardised object
+            band_combo_map = standardise_BandCombo(row, bird, study_area)
 
             try:
                 band_combo = BandCombo.objects.get(bird=bird)
