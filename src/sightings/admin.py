cd7a4af4e8a0137ba4a5b964b4f04af16805c85c
George Moon <george@moon.org.nz>
2017-07-06 16:52:41 +1200

    First pass at implementing sightings models

---
+++ src/sightings/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from .models.contributors import SightingsContributor
+from .models.media import SightingsMedia
+from .models.sightings import SightingsNonSighting, SightingsSighting, SightingsBird
+
+admin.site.register(SightingsContributor)
+admin.site.register(SightingsMedia)
+
+admin.site.register(SightingsNonSighting)
+admin.site.register(SightingsSighting)
+admin.site.register(SightingsBird)
---
+++ src/sightings/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# TODO: write tests for SightingsContributor
+# TODO: write tests for SightingsMedia
+# TODO: write tests for SightingsNonSighting
+# TODO: write tests for SightingsSighting
+# TODO: write tests for SightingsBird
