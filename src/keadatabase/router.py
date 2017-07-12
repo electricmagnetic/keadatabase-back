b603b2422f1d34a957d0fc657f9f1eb319e79d7a
George Moon <george@moon.org.nz>
2017-07-12 13:24:43 +1200

    Abstracted DRF router into separate file

---
+++ src/keadatabase/router.py
@@ -0,0 +1,23 @@
+""" DRF router configuration """
+from rest_framework.routers import DefaultRouter
+
+from locations.views import StudyAreaViewSet, RegionViewSet, CommonLocationViewSet
+from birds.views import BirdViewSet
+from bands.views import BandComboViewSet
+from sightings.views import SightingsSightingViewSet
+
+router = DefaultRouter()
+
+router.register(r'band_combos', BandComboViewSet, 'BandCombo')
+
+router.register(r'birds', BirdViewSet, 'Bird')
+
+router.register(r'locations/study_areas', StudyAreaViewSet, 'StudyArea')
+router.register(r'locations/regions', RegionViewSet, 'Region')
+router.register(r'locations/common_locations', CommonLocationViewSet, 'CommonLocation')
+
+#router.register(r'report', ReportViewSet)
+
+#router.register(r'sightings/sightings', SightingsSightingViewSet, 'SightingsSighting')
+#router.register(r'sightings/non-sightings', SightingsNonSightingsViewSet, 'SightingsNonSighting')
+#router.register(r'sightings/birds', SightingsBirdViewSet, 'SightingsBird')
