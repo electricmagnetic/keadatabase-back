0ba29189c9ca725d35a5a589a0535b8ec1d32550
George Moon <george@moon.org.nz>
2017-07-14 14:31:08 +1200

    Added basic admin config to Sightings to make it easier to moderate.

--- src/sightings/admin.py
+++ src/sightings/admin.py
@@ -5,9 +5,20 @@ from .models.contributors import SightingsContributor
 from .models.sightings import SightingsNonSighting, SightingsSighting
 from .models.birds import SightingsBird
 
+class SightingsBirdInline(admin.TabularInline):
+    model = SightingsBird
+    extra = 0
+
+class SightingsSightingAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'quality',)
+    inlines = [SightingsBirdInline]
+
+class SightingsNonSightingAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'quality',)
+
 admin.site.register(SightingsContributor)
 #admin.site.register(SightingsMedia)
 
-admin.site.register(SightingsNonSighting)
-admin.site.register(SightingsSighting)
+admin.site.register(SightingsNonSighting, SightingsNonSightingAdmin)
+admin.site.register(SightingsSighting, SightingsSightingAdmin)
 admin.site.register(SightingsBird)
