62fd0f954919dc1c4dce8de413c3ebfad5aeba74
George Moon <george@moon.org.nz>
2017-08-13 14:25:26 +1200

    Added more fields to Sightings/NonSightings list in admin.

--- src/sightings/admin.py
+++ src/sightings/admin.py
@@ -10,11 +10,11 @@ class SightingsBirdInline(admin.TabularInline):
     extra = 0
 
 class SightingsSightingAdmin(admin.OSMGeoAdmin):
-    list_display = ('__str__', 'quality',)
+    list_display = ('__str__', 'contributor', 'region', 'quality', 'date_created',)
     inlines = [SightingsBirdInline]
 
 class SightingsNonSightingAdmin(admin.ModelAdmin):
-    list_display = ('__str__', 'quality',)
+    list_display = ('__str__', 'contributor', 'region', 'quality', 'date_created',)
 
 admin.site.register(SightingsContributor)
 #admin.site.register(SightingsMedia)
