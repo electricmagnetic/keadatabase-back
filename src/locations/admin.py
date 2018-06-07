50b86913738423c2f13d8683ce4bc4baa348ddd7
George Moon <george@catalyst.net.nz>
2018-06-07 17:16:12 +1200

    Add Place model

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -1,7 +1,8 @@
 from django.contrib.gis import admin
 
-from .models import StudyArea, Region, CommonLocation
+from .models import StudyArea, Region, CommonLocation, Place
 
 admin.site.register(StudyArea)
 admin.site.register(Region)
 admin.site.register(CommonLocation, admin.OSMGeoAdmin)
+admin.site.register(Place, admin.OSMGeoAdmin)
