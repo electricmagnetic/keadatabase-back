5b7384146c01f2ea1b0c5e583bb11c3852083727
George Moon <george@moon.org.nz>
2018-06-10 22:10:35 +1200

    Changed from protect to set null

--- src/sightings/models/sightings.py
+++ src/sightings/models/sightings.py
@@ -37,7 +37,7 @@ class SightingsBase(models.Model):
     date_sighted = models.DateField()
     time_sighted = models.TimeField()
 
-    region = models.ForeignKey(Region, on_delete=models.PROTECT, blank=True, null=True)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, blank=True, null=True)
 
     # Optional
     comments = models.TextField(blank=True)
