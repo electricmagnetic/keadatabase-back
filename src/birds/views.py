c5edd6afe70b7b523aec5065c06ad388878f5707
George Moon <georgemoon@users.noreply.github.com>
2018-06-26 13:45:04 +1200

    Added random ordering to faciliate selection of featured birds (#86)

--- src/birds/views.py
+++ src/birds/views.py
@@ -5,6 +5,19 @@ from keadatabase.pagination import BirdPagination
 from .models import Bird
 from .serializers import BirdSerializer
 
+class BirdOrdering(django_filters.OrderingFilter):
+    def __init__(self, *args, **kwargs):
+        super(BirdOrdering, self).__init__(*args, **kwargs)
+        self.extra['choices'] += [
+            ('random', 'Random')
+        ]
+
+    def filter(self, qs, value):
+        if any(v in ['random'] for v in value):
+            return qs.order_by('?')
+
+        return super(BirdOrdering, self).filter(qs, value)
+
 class BirdFilter(django_filters.FilterSet):
     is_extended = django_filters.BooleanFilter(name='bird_extended__is_extended',
                                                lookup_expr='isnull',
@@ -14,6 +27,8 @@ class BirdFilter(django_filters.FilterSet):
                                                label='Is featured')
 
     has_band = django_filters.BooleanFilter(name='band_combo', lookup_expr='isnull', exclude=True)
+
+    ordering = BirdOrdering(fields=('name', 'status', 'study_area', 'bird_extended',))
 
     class Meta:
         model = Bird
