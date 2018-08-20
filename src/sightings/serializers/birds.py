2f3498303b24ff7279c568c39bd471ab59dcaf16
George Moon <georgemoon@users.noreply.github.com>
2018-08-20 12:41:07 +1200

    Added Bird object to SightingsBird serializer (#88)

--- src/sightings/serializers/birds.py
+++ src/sightings/serializers/birds.py
@@ -2,13 +2,15 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import GeometryField
 
 from ..models.birds import SightingsBird
+from birds.serializers import BirdSerializer
 
 class SightingsBirdSerializer(serializers.ModelSerializer):
     get_banded_display = serializers.CharField()
     get_sex_guess_display = serializers.CharField()
     get_life_stage_guess_display = serializers.CharField()
 
-    get_bird_display = serializers.StringRelatedField(source='bird', many=False)
+    get_bird_display = serializers.StringRelatedField(source='bird', many=False) # TODO: deprecate
+    bird = BirdSerializer(many=False, read_only=True)
     sighting = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
 
     sighting__date_sighted = serializers.ReadOnlyField(source='sighting.date_sighted')
@@ -18,3 +20,9 @@ class SightingsBirdSerializer(serializers.ModelSerializer):
     class Meta:
         model = SightingsBird
         fields = '__all__'
+
+    @staticmethod
+    def setup_eager_loading(queryset):
+        queryset = queryset.prefetch_related("bird", "bird__band_combo", "bird__study_area", "bird__bird_extended", "sighting",)
+
+        return queryset
