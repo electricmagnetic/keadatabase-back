989fc7e466a28bb448e0ea27b5875126d19dc64b
George Moon <georgemoon@users.noreply.github.com>
2019-02-13 15:15:26 +1300

    Adding extra searching and filtering abilities to the admin interface (#90)

--- src/bands/admin.py
+++ src/bands/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 
 from .models import BandCombo
 
-admin.site.register(BandCombo)
+class BandComboAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'bird', 'study_area', 'date_modified',)
+    search_fields = ('name__icontains','bird__name',)
+
+admin.site.register(BandCombo, BandComboAdmin)
