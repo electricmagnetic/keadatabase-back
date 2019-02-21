925e9dd4a96935e9cbf592f78902b42f82ffd26a
George Moon <george@catalyst.net.nz>
2019-02-22 11:50:15 +1300

    Improving band/bird list in admin

--- src/bands/admin.py
+++ src/bands/admin.py
@@ -5,5 +5,6 @@ from .models import BandCombo
 class BandComboAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'bird', 'study_area', 'date_modified',)
     search_fields = ('name__icontains','bird__name',)
+    list_per_page = 250
 
 admin.site.register(BandCombo, BandComboAdmin)
--- src/birds/admin.py
+++ src/birds/admin.py
@@ -3,7 +3,13 @@ from django.contrib import admin
 from .models import Bird, BirdExtended
 
 class BirdAdmin(admin.ModelAdmin):
-    search_fields = ('name',)
+    search_fields = ('name', 'primary_band',)
+    list_display = ('__str__', 'primary_band', 'band_combo', 'study_area',)
+    list_per_page = 250
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.all().select_related("bird_extended", "study_area", "band_combo")
 
 class BirdExtendedAdmin(admin.ModelAdmin):
     list_filter = ('is_featured',)
