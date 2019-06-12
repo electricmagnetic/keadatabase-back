859c3d29eb98c1b47865188488abe28cc2bfaf5e
George Moon <george@moon.org.nz>
2019-06-12 12:25:50 +1200

    Added GridTile model and import script

--- src/locations/admin.py
+++ src/locations/admin.py
@@ -1,7 +1,23 @@
 from django.contrib.gis import admin
 
-from .models import StudyArea, Region, Place
+from .models import StudyArea, Region, Place, GridTile
+
+class GridTileAdmin(admin.ModelAdmin):
+    """ Read only view of grid tiles """
+
+    list_display = ('id',)
+    search_fields = ('id',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
 
 admin.site.register(StudyArea)
 admin.site.register(Region)
 admin.site.register(Place, admin.OSMGeoAdmin)
+admin.site.register(GridTile, GridTileAdmin)
