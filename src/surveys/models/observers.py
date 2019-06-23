49731ae785459423a28ae989e53074ae07db62e2
George Moon <george@moon.org.nz>
2019-06-24 10:50:17 +1200

    Added more purposes

--- src/surveys/models/observers.py
+++ src/surveys/models/observers.py
@@ -4,12 +4,14 @@ PURPOSE_CHOICES = (
     ('', ''),
     ('tunnel', 'Tracking Tunnel Check'),
     ('fwf', 'FWF Hunting'),
-    ('guide', 'Adventure Guiding'),
-    ('trap', 'Pest Trapping'),
-    ('permolat', 'Permolat/Remote Hut Work'),
-    ('tramp', 'Tramping'),
     ('hunt', 'Hunting'),
+    ('guide', 'Guiding'),
+    ('trap', 'Trapping'),
+    ('permolat', 'Permolat/Hut/Track Work'),
+    ('tramp', 'Tramping'),
     ('research', 'Researching'),
+    ('kea', 'Kea Surveying'),
+    ('hut', 'Hut Wardening'),
     ('other', 'Other'),
 )
 
