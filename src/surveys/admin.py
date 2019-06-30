80e65bfd51675934b538b342d4fa72542038bbe3
George Moon <george@moon.org.nz>
2019-06-30 18:19:27 +1200

    Survey admin optimisation

--- src/surveys/admin.py
+++ src/surveys/admin.py
@@ -9,16 +9,35 @@ class ObserverAdmin(admin.ModelAdmin):
 class SurveyHourInline(admin.StackedInline):
     model = SurveyHour
     extra = 0
+    list_select_related = True
+    raw_id_fields = ('grid_tile',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.all(). \
+            select_related('survey', 'grid_tile'). \
+            all()
 
 class SurveyAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'date', 'max_flock_size', 'observer', 'status',)
     list_filter = ('date', 'status',)
     inlines = [SurveyHourInline]
     search_fields = ('id__exact',)
+    list_select_related = True
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.all().select_related('observer').prefetch_related('hours')
 
 class SurveyHourAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'survey', 'hour', 'kea', 'activity', 'grid_tile',)
     list_filter = ('kea', 'activity',)
+    list_select_related = True
+    raw_id_fields = ('grid_tile',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.all().select_related('survey', 'grid_tile',)
 
 admin.site.register(Observer, ObserverAdmin)
 admin.site.register(Survey, SurveyAdmin)
