663a594ae817cba35afbd937bcc29efe9cedb453
George Moon <georgemoon@users.noreply.github.com>
2019-11-19 21:37:58 +0100

    Added bird sighting GeoJSON (#118)

--- src/geojson/views.py
+++ src/geojson/views.py
@@ -1,9 +1,10 @@
 from rest_framework import renderers
 
 from sightings.views.sightings import SightingViewSet
+from sightings.views.birds import BirdSightingViewSet
 from locations.views import GridTileViewSet
-from keadatabase.pagination import SightingGeoJSONPagination, GridTileGeoJSONPagination
-from .serializers import SightingGeoJSONSerializer, GridTileGeoJSONSerializer
+from keadatabase.pagination import SightingGeoJSONPagination, GridTileGeoJSONPagination, BirdSightingGeoJSONPagination
+from .serializers import SightingGeoJSONSerializer, GridTileGeoJSONSerializer, BirdSightingGeoJSONSerializer
 
 class SightingGeoJSONViewSet(SightingViewSet):
     serializer_class = SightingGeoJSONSerializer
@@ -15,6 +16,13 @@ class SightingGeoJSONViewSet(SightingViewSet):
 class GridTileGeoJSONViewSet(GridTileViewSet):
     serializer_class = GridTileGeoJSONSerializer
     pagination_class = GridTileGeoJSONPagination
+
+    # Disable HTML view of this for compatibility
+    renderer_classes = [renderers.JSONRenderer]
+
+class BirdSightingGeoJSONViewSet(BirdSightingViewSet):
+    serializer_class = BirdSightingGeoJSONSerializer
+    pagination_class = BirdSightingGeoJSONPagination
 
     # Disable HTML view of this for compatibility
     renderer_classes = [renderers.JSONRenderer]
