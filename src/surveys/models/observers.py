31077c384e3a83e55d1058ae64873be2c5ff1e55
George Moon <george@moon.org.nz>
2020-03-18 21:21:03 +0100

    Removing purpose from observer

--- src/surveys/models/observers.py
+++ src/surveys/models/observers.py
@@ -1,14 +1,9 @@
 from django.db import models
 
-from .choices import PURPOSE_CHOICES
-
 class Observer(models.Model):
     """ Observer details for a particular survey """
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
-
-    # Optional
-    purpose = models.CharField(max_length=15, blank=True, choices=PURPOSE_CHOICES, default='')
 
     def __str__(self):
         return self.name
--- src/surveys/models/surveys.py
+++ src/surveys/models/surveys.py
@@ -2,8 +2,22 @@ from django.db import models
 from django.core.exceptions import ValidationError
 
 from .observers import Observer
-from .choices import PURPOSE_CHOICES
 from locations.models import GridTile
+
+PURPOSE_CHOICES = (
+    ('', ''),
+    ('tunnel', 'Tracking Tunnel Check'),
+    ('fwf', 'FWF Hunting'),
+    ('hunt', 'Hunting'),
+    ('guide', 'Guiding'),
+    ('trap', 'Trapping'),
+    ('permolat', 'Permolat/Hut/Track Work'),
+    ('tramp', 'Tramping'),
+    ('research', 'Researching'),
+    ('kea', 'Kea Surveying'),
+    ('hut', 'Hut Wardening'),
+    ('other', 'Other'),
+)
 
 ACTIVITY_CHOICES = (
     ('W', 'W - Walking'),
