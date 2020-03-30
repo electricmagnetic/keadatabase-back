b6433cbb1d6a54758d033de305e29b50b66f1033
George Moon <george@moon.org.nz>
2020-03-31 11:34:22 +1300

    Adding full sighting information to bird_sighting

--- src/sightings/serializers/birds.py
+++ src/sightings/serializers/birds.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework_gis.serializers import GeometryField
 
 from ..models.birds import BirdSighting
+from .sightings import SightingSerializer
 from birds.serializers import BirdSerializer
 
 class BirdSightingSerializer(serializers.ModelSerializer):
@@ -10,8 +11,9 @@ class BirdSightingSerializer(serializers.ModelSerializer):
     get_life_stage_guess_display = serializers.CharField()
 
     bird = BirdSerializer(many=False, read_only=True)
-    sighting = serializers.PrimaryKeyRelatedField(many=False, read_only=True)
+    sighting = SightingSerializer(many=False, read_only=True)
 
+    # TODO: remove these three fields. Currently left in for backwards compatibility.
     sighting__date_sighted = serializers.ReadOnlyField(source='sighting.date_sighted')
     sighting__time_sighted = serializers.ReadOnlyField(source='sighting.time_sighted')
     sighting__point_location = GeometryField(source='sighting.point_location')
@@ -22,6 +24,13 @@ class BirdSightingSerializer(serializers.ModelSerializer):
 
     @staticmethod
     def setup_eager_loading(queryset):
-        queryset = queryset.prefetch_related("bird", "bird__band_combo", "bird__study_area", "bird__bird_extended", "sighting",)
+        queryset = queryset.prefetch_related(
+            "bird",
+            "bird__band_combo",
+            "bird__study_area",
+            "bird__bird_extended",
+            "sighting",
+            "sighting__contributor",
+        )
 
         return queryset
