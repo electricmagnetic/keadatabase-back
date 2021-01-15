d30f82548a6d550a09a54ca5f749a49159022466
George Moon <george@moon.org.nz>
2021-01-15 15:20:01 +1300

    Added Sentry

--- src/keadatabase/settings.py
+++ src/keadatabase/settings.py
@@ -329,3 +329,16 @@ LEAFLET_CONFIG = {
     'TILES': [],
 }
 # yapf: enable
+
+# Sentry
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn=env.str('SENTRY_DSN', ''),
+    integrations=[DjangoIntegration()],
+    traces_sample_rate=1.0,
+    send_default_pii=True,
+)
+
